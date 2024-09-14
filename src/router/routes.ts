07c2cb6f5b9b1683de849c1184873f9d7a8a94e5
Johan1707 <johandavid1707@gmail.com>
2024-09-14 00:34:41 -0500

    Insertion of header and translation security middlewares

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,3 +1,15 @@
 import { Hono } from 'hono'
+import { secureHeaders } from 'hono/secure-headers'
 
-export const app = new Hono().basePath('./api/v1.0')
+import { i18nMiddleware } from '@/middlewares'
+
+export const app = new Hono().basePath('/api/v1.0')
+
+app.use(
+    secureHeaders(),
+    i18nMiddleware
+)
+
+app.post('/menus', (c) => {
+    return c.json({ message: 'Hola mundo'})
+})

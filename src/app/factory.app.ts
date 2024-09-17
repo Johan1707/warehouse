e57d151b3f04195eff3c54e849c3da9087703137
Johan1707 <johandavid1707@gmail.com>
2024-09-17 14:57:43 -0500

    feat: modify the type of variables used by the initial factory parameterization to create an app

--- src/app/factory.app.ts
+++ src/app/factory.app.ts
@@ -3,9 +3,11 @@ import { PrismaClient } from '@prisma/client'
 import { PrismaD1 } from '@prisma/adapter-d1'
 import type { Context, Next, Hono } from 'hono'
 
+import type { Env } from '@/types'
+
 
 export default createFactory({
-    initApp: (app: Hono<{ Bindings: CloudflareBindings }>) => {
+    initApp: (app: Hono<Env>) => {
         app.use(async (c: Context, next: Next) => {
             const adapter = new PrismaD1(c.env.DB)
             const db = new PrismaClient({ adapter })

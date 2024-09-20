f6bd3c78cf8937095d2529a908a6253348b6aa5c
Johan1707 <johandavid1707@gmail.com>
2024-09-20 11:51:07 -0500

    feat(menu): add handler to send all menus

--- src/handlers/menu.handler.ts
+++ src/handlers/menu.handler.ts
@@ -4,7 +4,7 @@ import type { StatusCode } from 'hono/utils/http-status'
 import type { Menu } from '@prisma/client'
 
 import { factoryDB } from '@/app'
-import { createMenuService } from '@/services'
+import { createMenuService, getAllMenusService } from '@/services'
 import type { Env, MenuResponse } from '@/types'
 
 export const createMenuHandler = factoryDB.createHandlers(async (c: Context<Env>): Promise<Response> => {
@@ -17,4 +17,15 @@ export const createMenuHandler = factoryDB.createHandlers(async (c: Context<Env>
     }
 
     return c.json(menu, 201 as StatusCode)
+})
+
+export const getAllMenusHandler = factoryDB.createHandlers(async (c: Context<Env>): Promise<Response> => {
+    const t = useTranslation(c)
+    const [error, menus]: MenuResponse = await getAllMenusService(c.var.db)
+
+    if(error) {
+        return c.json({ message: t(error.message) }, error.status as StatusCode)
+    }
+
+    return c.json(menus, 200 as StatusCode)
 })

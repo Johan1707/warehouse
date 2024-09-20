75fbd4dcd8d0d2f279a3654048c7f2418ddf8270
Johan1707 <johandavid1707@gmail.com>
2024-09-20 12:35:31 -0500

    add validation schema for updating a menu

--- src/schemas/menu.schema.ts
+++ src/schemas/menu.schema.ts
@@ -32,3 +32,31 @@ export const CreateMenuSchema = object({
     menu_id: optional( number() ),
     active: boolean()
 })
+
+export const UpdateMenuSchema = object({
+    title: optional(
+        pipe(
+            string(),
+            nonEmpty(),
+            minLength(4),
+            maxLength(80)
+        )
+    ),
+    route: optional(
+        pipe(
+            string(),
+            maxLength(120)
+        )
+    ),
+    icon: optional(
+        pipe(
+            string(),
+            nonEmpty(),
+            maxLength(60)        
+        )
+    ),
+    sequence: optional( number() ),
+    style: optional( string() ),
+    menu_id: optional( number() ),
+    active: optional( boolean() )
+})

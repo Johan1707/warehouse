83d0a0b7e7f99230f7a05bccc2f4c2b3fe252f23
Johan1707 <johandavid1707@gmail.com>
2024-09-20 11:52:57 -0500

    feat(menu): add path to get all menus

--- src/router/v1/menu.route.ts
+++ src/router/v1/menu.route.ts
@@ -4,9 +4,10 @@ import { factoryDB } from '@/app'
 import type { Env } from '@/types'
 import { validateRequest } from '@/middlewares'
 import { CreateMenuSchema } from '@/schemas'
-import { createMenuHandler } from '@/handlers'
+import { createMenuHandler, getAllMenusHandler } from '@/handlers'
 
 export const appMenu: Hono<Env> = factoryDB.createApp().basePath('/menus')
 
 appMenu
     .post('/', validateRequest('json', CreateMenuSchema), ...createMenuHandler)
+    .get('/', ...getAllMenusHandler)

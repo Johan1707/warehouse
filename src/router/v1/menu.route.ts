12dcabbb07168b8a7a6ec68b18dbe438f2bc607d
Johan1707 <johandavid1707@gmail.com>
2024-09-20 12:39:22 -0500

    feat(menu): add path to get menu by id

--- src/router/v1/menu.route.ts
+++ src/router/v1/menu.route.ts
@@ -3,11 +3,16 @@ import { factoryDB } from '@/app'
 
 import type { Env } from '@/types'
 import { validateRequest } from '@/middlewares'
-import { CreateMenuSchema } from '@/schemas'
-import { createMenuHandler, getAllMenusHandler } from '@/handlers'
+import { CreateMenuSchema, IdSchema, UpdateMenuSchema } from '@/schemas'
+import {
+    createMenuHandler,
+    getAllMenusHandler,
+    getMenuByIdHandler
+} from '@/handlers'
 
 export const appMenu: Hono<Env> = factoryDB.createApp().basePath('/menus')
 
 appMenu
     .post('/', validateRequest('json', CreateMenuSchema), ...createMenuHandler)
     .get('/', ...getAllMenusHandler)
+    .get('/', validateRequest('param', IdSchema), ...getMenuByIdHandler)
